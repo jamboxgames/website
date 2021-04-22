fafa2052a542cb19cc258a2bccaedcfd006286d7
ravi-jam <80817600+ravi-jam@users.noreply.github.com>
2021-04-22 12:39:20 +0530

    Update site.js

--- _data/site.js
+++ _data/site.js
@@ -1,4 +1,4 @@
 module.exports = {
-   url: process.env.ELEVENTY_ENV === 'development' ? 'http://127.0.0.1:8000' :  process.env.ELEVENTY_ENV === 'production' ? 'https://jambox.games' : 'https://elegant-hopper-04dc90.netlify.app'
+   url: process.env.ELEVENTY_ENV === 'development' ? 'http://127.0.0.1:8000' :  process.env.ELEVENTY_ENV === 'staging' ? 'https://elegant-hopper-04dc90.netlify.app' : 'https://jambox.games'
    
   };
